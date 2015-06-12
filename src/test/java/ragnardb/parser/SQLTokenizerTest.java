b79c219d2f248473fa5e5efbf14d482e60a2e149
Kyle Moore <kmoore@guidewire.com>
2015-06-12 13:35:22 -0700

    Moving test dir around

---
+++ src/test/java/ragnardb/parser/SQLTokenizerTest.java
@@ -0,0 +1,21 @@
+package ragnardb.parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+public class SQLTokenizerTest
+{
+  @Test
+  public void basicTokenizingTest() {
+    File inFile = new File("src/test/resources/Foo/Users.dll");
+    System.out.println(inFile.getAbsolutePath());
+    Assert.assertTrue(inFile.exists());
+
+
+
+  }
+
+
+}
