b115dd7a7c6ff1fdcdcb7cbeaa6eb47507af86df
Kyle Moore <kmoore@guidewire.com>
2015-06-16 17:49:05 -0700

    Lazily prepare cache of types; fun with Streams API

--- src/main/java/ragnardb/plugin/SQLPlugin.java
+++ src/main/java/ragnardb/plugin/SQLPlugin.java
@@ -1,7 +1,6 @@
 package ragnardb.plugin;
 
 import gw.fs.IDirectory;
-import gw.fs.IFile;
 import gw.internal.gosu.util.StringUtil;
 import gw.lang.reflect.IType;
 import gw.lang.reflect.RefreshKind;
@@ -9,41 +8,63 @@ import gw.lang.reflect.TypeLoaderBase;
 import gw.lang.reflect.TypeSystem;
 import gw.lang.reflect.module.IModule;
 import gw.util.Pair;
+import gw.util.concurrent.LockingLazyVar;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SQLPlugin extends TypeLoaderBase {
 
   private static final String FILE_EXTENSION = ".ddl";
 
-  Map<String, ISQLSource> _sqlSourcesByPackage;
+  private List<Pair<String, ISQLSource>> _sqlSources;
+
+  private final LockingLazyVar<Map<String, ISQLSource>> _sqlSourcesByPackage = new LockingLazyVar<Map<String, ISQLSource>>() {
+    @Override
+    protected Map<String, ISQLSource> init() {
+      Map<String, ISQLSource> result = new HashMap<>(_sqlSources.size());
+
+      for(Pair<String, ISQLSource> ddlFilePair : _sqlSources) {
+        String fileName = ddlFilePair.getFirst();
+        String packageName = fileName.substring(0, fileName.length() - FILE_EXTENSION.length()).replace('/', '.');
+        ISQLSource ddlFile = ddlFilePair.getSecond();
+        Set<String> typeNames = ddlFile.getTypeNames();
+
+        boolean allTypeNamesAreValid = true;
+        for(String typeName : typeNames) {
+          String fullyQualifiedName = packageName + '.' + typeName;
+          if(!isValidTypeName(fullyQualifiedName)) {
+            allTypeNamesAreValid = false;
+          }
+          if(allTypeNamesAreValid) {
+            result.put(packageName, ddlFile);
+          }
+        }
+      }
+      return result;
+    }
+  };
 
   public SQLPlugin(IModule module) {
     super(module);
-    List<Pair<String, IFile>> ddlFiles = module.getFileRepository().findAllFilesByExtension(FILE_EXTENSION);
-    final int initialCapacity = ddlFiles.size();
-    Map<String, ISQLSource> result = new HashMap<>(initialCapacity);
+    initSources(module);
+  }
 
-    for(Pair<String, IFile> pair : ddlFiles) {
-      String fileName = pair.getFirst();
-      String packageName = fileName.substring(0, fileName.length() - FILE_EXTENSION.length()).replace('/', '.');
-      ISQLSource sqlSource = new SQLSource(pair.getSecond().getPath());
-      Set<String> typeNames = sqlSource.getTypeNames();
-
-      boolean allTypeNamesAreValid = true;
-      for(String typeName : typeNames) {
-        String fullyQualifiedName = packageName + '.' + typeName;
-        if(!isValidTypeName(fullyQualifiedName)) {
-          allTypeNamesAreValid = false;
-        }
-        if(allTypeNamesAreValid) {
-          result.put(packageName, sqlSource);
-        }
-      }
-    }
-
-    _sqlSourcesByPackage = result; //TODO replace with lockinglazyvar impl
-
+  /**
+   * Populates _sqlSources from the provided IModule
+   * @param module Find *.ddl files in this module
+   */
+  private void initSources(IModule module) {
+    //leverage Streams API to basically cast Pair<String, IFile> to Pair<String, ISQLSource> in place
+    _sqlSources = module.getFileRepository().findAllFilesByExtension(FILE_EXTENSION)
+        .stream()
+        .map(pair -> new Pair<String, ISQLSource>(pair.getFirst(), new SQLSource(pair.getSecond().getPath())))
+        .collect(Collectors.toList());
   }
 
   @Override
@@ -53,8 +74,8 @@ public class SQLPlugin extends TypeLoaderBase {
     String typeName = packagesAndType[packagesAndType.length - 1];
     String packageName = String.join(".", packages);
 
-    if(_sqlSourcesByPackage.keySet().contains(packageName)) {
-      ISQLSource ddlFile = new SQLSource(_sqlSourcesByPackage.get(packageName).getPath());
+    if(_sqlSourcesByPackage.get().keySet().contains(packageName)) {
+      ISQLSource ddlFile = new SQLSource(_sqlSourcesByPackage.get().get(packageName).getPath());
       if(ddlFile.getTypeNames().contains(typeName)) {
         return TypeSystem.getOrCreateTypeReference(new SQLType(this, fullyQualifiedName));
       }
