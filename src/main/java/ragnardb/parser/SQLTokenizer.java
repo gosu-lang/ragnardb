00f13c7cc1362484ae835a5d773b1430f88ac06f
freddylukai <kail@princeton.edu>
2015-06-17 11:03:36 -0700

    Updated Number and Commenting

--- src/main/java/ragnardb/parser/SQLTokenizer.java
+++ src/main/java/ragnardb/parser/SQLTokenizer.java
@@ -5,213 +5,261 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class SQLTokenizer {
-  private BufferedReader reader;
-  private int line;
-  private int col;
-  private char ch;
-  private boolean EOF;
+    private BufferedReader reader;
+    private int line;
+    private int col;
+    private char ch;
+    private boolean EOF;
 
-  public SQLTokenizer(Reader r) {
-    reader = new BufferedReader(r);
-    line = 1;
-    col = 0;
-    EOF = false;
-    next();
-  }
-
-  private boolean isBlank(char c) {
-    return c == ' ' || c == '\t' || c == '\n' || c == '\r';
-  }
-
-  private boolean isIdent(char c) {
-    return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_';
-  }
-  private void hComment(){
-    while(ch != '\n')
-    {next();}
-    next();
-  }
-  private void comment(){
-    next();
-    while(ch != '*') {
-      next();
-      if(ch == '*') {
+    public SQLTokenizer(Reader r) {
+        reader = new BufferedReader(r);
+        line = 1;
+        col = 0;
+        EOF = false;
         next();
-        if(ch == '/') {
-          next();
-          return;
+    }
+
+    private boolean isBlank(char c) {
+        return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+    }
+
+    private boolean isIdent(char c) {
+        return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_';
+    }
+
+    private void hComment() {
+        while (ch != '\n') {
+            next();
         }
-      }
-    }
-  }
-
-  public Token get() {
-    Token tok;
-    while (!EOF && isBlank(ch)) {
-      next();
+        next();
     }
 
-    if (EOF) {
-      tok = new Token(TokenType.EOF, line, col);
-    } else if (ch == '/') {
-      next();
-      if (ch == '*') {
-        comment();
-        return get();
-      } else {
-        tok = new Token(TokenType.SLASH, line, col - 1);
-      }
-    }
-    else if (ch == '-') {
-      next();
-      if (ch == '-') {
-      hComment();
-      return get();
-      } else {
-        tok = new Token(TokenType.MINUS, line, col - 1);
+    private void comment() {
+        boolean exiting = false;
+        while (!exiting) {
+            next();
+            if (ch == '*') {
+                next();
+                if (ch == '/') {
+                    next();
+                    exiting = true;
+                }
+            }
+            if (EOF) {
+                exiting = true;
+            }
+        }
     }
 
 
-    }  else if (isIdent(ch)) {
-      tok = identifier();
-    } else if (isNumberOrDot(ch)) {
-      tok = numberOrDot();
-    } else if (ch == '(') {
-      tok = new Token(TokenType.LPAREN, line, col);
-      next();
-    } else if (ch == ')') {
-      tok = new Token(TokenType.RPAREN, line, col);
-      next();
-    } else if (ch == '+') {
-      tok = new Token(TokenType.PLUS, line, col);
-      next();
-    } else if (ch == ',') {
-      tok = new Token(TokenType.COMMA, line, col);
-      next();
-    } else if (ch == ';') {
-      tok = new Token(TokenType.SEMI, line, col);
-      next();
-    } else {
-      tok = new Token(TokenType.UNKNOWN, line, col);
-      next();
+    public Token get() {
+        Token tok;
+        while (!EOF && isBlank(ch)) {
+            next();
+        }
+
+        if (EOF) {
+            tok = new Token(TokenType.EOF, line, col);
+        } else if (ch == '/') {
+            next();
+            if (ch == '*') {
+                comment();
+                return get();
+            } else {
+                tok = new Token(TokenType.SLASH, line, col - 1);
+            }
+        } else if (ch == '-') {
+            next();
+            if (ch == '-') {
+                hComment();
+                return get();
+            } else {
+                tok = new Token(TokenType.MINUS, line, col - 1);
+            }
+        } else if (ch == '"') {
+            tok = stringLiteralIdentifier();
+        } else if (isIdent(ch)) {
+            tok = identifier();
+        } else if (isNumberOrDot(ch)) {
+            tok = numberOrDot();
+        } else if (ch == '(') {
+            tok = new Token(TokenType.LPAREN, line, col);
+            next();
+        } else if (ch == ')') {
+            tok = new Token(TokenType.RPAREN, line, col);
+            next();
+        } else if (ch == '+') {
+            tok = new Token(TokenType.PLUS, line, col);
+            next();
+        } else if (ch == ',') {
+            tok = new Token(TokenType.COMMA, line, col);
+            next();
+        } else if (ch == ';') {
+            tok = new Token(TokenType.SEMI, line, col);
+            next();
+        } else {
+            tok = new Token(TokenType.UNKNOWN, line, col);
+            next();
+        }
+        return tok;
     }
-    return tok;
-  }
 
-  private boolean isNumberOrDot(char c) {
-    return (c >= '0' && c <= '9') || c == '.';
-  }
+    private boolean isNumberOrDot(char c) {
+        return (c >= '0' && c <= '9') || c == '.';
+    }
 
-  private Token numberOrDot() {
-    Token tok;
-    int l = line;
-    int c = col;
-    long intNum = 0;
-    double decNum;
-    boolean isDecimal = false;
-    boolean isExponential = false;
+    private Token numberOrDot() {
+        Token tok;
+        int l = line;
+        int c = col;
+        long intNum = 0;
+        double decNum;
+        boolean isDecimal = false;
 
     /*Set exponent*/
-    int e = 0;
+        int e = 0;
 
-    while(isNumberOrDot(ch) || ch == 'e') {
-      if(ch == '.') {
-        isDecimal = true;
-        next();
-        if(!isNumberOrDot(ch) && intNum == 0){
-          return new Token(TokenType.DOT, line, col-1);
+        while (isNumberOrDot(ch)) {
+
+            if (ch == '.' && !isDecimal) {
+                isDecimal = true;
+                next();
+                if (!isNumberOrDot(ch) && intNum == 0) {
+                    return new Token(TokenType.DOT, line, col - 1);
+                }
+            }
+
+            if (!EOF) {
+                intNum = intNum * 10 + (ch - '0');
+                if (isDecimal) {
+                    e--;
+                }
+                next();
+            }
+
+            if (ch == '.' && isDecimal) {
+                decNum = intNum * Math.pow(10, e);
+                tok = new Token(TokenType.DOUBLE, l, c);
+                tok.setDoubleNumber(decNum);
+                return tok;
+            }
         }
-        continue;
-      }
-      if(ch == 'e') {
-        isExponential = true;
-        next();
-        break;
-      }
-      intNum = intNum * 10 + (ch - '0');
-      if(isDecimal){ e--; }
-      next();
-    }
-    if(isExponential){
-      isDecimal = true;
+
+        if (ch == 'e' || ch == 'E') {
+            if (intNum == 0) {
+                intNum = 1;
+            }
+            next();
+            isDecimal = true;
       /*Used to deal with the event a negative exponential is used*/
-      int negativeExp = 1;
-      int expNum = 0;
-      if(ch == '+') {
+            int negativeExp = 1;
+            int expNum = 0;
+
+            if (ch == '+') {
+                next();
+            }
+            if (ch == '-') {
+                negativeExp = -1;
+                next();
+            }
+
+            while (isNumberOrDot(ch) && ch != '.') {
+                expNum = expNum * 10 + (ch - '0');
+                next();
+            }
+
+            if (ch == '.' || (!isNumberOrDot(ch) && !(EOF || isBlank(ch)))) {
+                e += negativeExp * expNum;
+                tok = new Token(TokenType.DOUBLE, l, c);
+                decNum = intNum * Math.pow(10, e);
+                tok.setDoubleNumber(decNum);
+                return tok;
+            }
+
+            e += negativeExp * expNum;
+        }
+        decNum = intNum * Math.pow(10, e);
+        if (isDecimal) {
+            tok = new Token(TokenType.DOUBLE, l, c);
+            tok.setDoubleNumber(decNum);
+        } else {
+            tok = new Token(TokenType.LONG, l, c);
+            tok.setLongNumber(intNum);
+        }
+        return tok;
+    }
+
+    private Token identifier() {
+        StringBuilder sb = new StringBuilder();
+        int l = line;
+        int c = col;
+
+
+        sb.append(ch);
         next();
-      }
-      if(ch == '-') {
-        negativeExp = -1;
+
+        while (isIdent(ch) || isNumberOrDot(ch)) {
+            sb.append(ch);
+            next();
+        }
+        String s = sb.toString().toLowerCase();
+        Token tok;
+        TokenType type = TokenType.find(s);
+        if (type != null) {
+            tok = new Token(type, l, c);
+        } else {
+            tok = new Token(TokenType.IDENT, l, c);
+            tok.setText(s);
+        }
+        return tok;
+    }
+
+
+    private Token stringLiteralIdentifier() {
+        StringBuilder sb = new StringBuilder();
+        int l = line;
+        int c = col;
+
         next();
-      }
-      while(isNumberOrDot(ch) && ch != '.') {
-        expNum = expNum * 10 + (ch - '0');
-        next();
-      }
-      e += negativeExp*expNum;
-    }
-    decNum = intNum * Math.pow(10, e);
-    if(isDecimal) {
-      tok = new Token(TokenType.DOUBLE, 1, c);
-      tok.setDoubleNumber(decNum);
-    } else {
-      tok = new Token(TokenType.LONG, 1, c);
-      tok.setLongNumber(intNum);
-    }
-    return tok;
-  }
 
-  private Token identifier() {
-    StringBuilder sb = new StringBuilder();
-    int l = line;
-    int c = col;
+        while (ch != '"') {
+            sb.append(ch);
+            next();
+        }
 
-
-    sb.append(ch);
-    next();
-
-    while(isIdent(ch) || isNumberOrDot(ch)  ) {
-      sb.append(ch);
-      next();
+        String s = sb.toString();
+        Token tok;
+        tok = new Token(TokenType.IDENT, l, c);
+        tok.setText(s);
+        return tok;
     }
-    String s = sb.toString().toLowerCase();
-    Token tok;
-    TokenType type = TokenType.find(s);
-    if(type != null) {
-      tok = new Token(type, l, c);
-    } else {
-      tok = new Token(TokenType.IDENT, l, c);
-      tok.setText(s);
-    }
-    return tok;
-  }
 
-  private void next() {
-    int c;
+    private void next() {
+        int c;
 
-    c = read();
-    if(c == '\r') {
-      c = read();
+        c = read();
+        if (c == '\r') {
+            c = read();
+        }
+        if (c == '\n') {
+            col = 1;
+            line++;
+        } else if (c != -1) {
+            col++;
+        } else {
+            EOF = true;
+            c = 0;
+        }
+        ch = (char) c;
     }
-    if(c == '\n') {
-      col = 1;
-      line++;
-    } else if(c != -1) {
-      col++;
-    } else {
-      EOF = true;
-      c = 0;
-    }
-    ch = (char) c;
-  }
 
-  private int read() {
-    int c;
-    try {
-      c = reader.read();
-    } catch(IOException e) {
-      c = -1;
+    private int read() {
+        int c;
+        try {
+            c = reader.read();
+        } catch (IOException e) {
+            c = -1;
+        }
+        return c;
     }
-    return c;
-  }
 }
