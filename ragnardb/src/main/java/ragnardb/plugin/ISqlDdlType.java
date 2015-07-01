4a2c79680d2d6656bf197c782dca6da2908961e8
Kyle Moore <kmoore@guidewire.com>
2015-07-01 15:27:04 -0700

    Change return type

--- ragnardb/src/main/java/ragnardb/plugin/ISqlDdlType.java
+++ ragnardb/src/main/java/ragnardb/plugin/ISqlDdlType.java
@@ -3,11 +3,9 @@ package ragnardb.plugin;
 import gw.lang.parser.IHasInnerClass;
 import gw.lang.reflect.IFileBasedType;
 import gw.lang.reflect.IType;
-import ragnardb.parser.ast.DDL;
 
-/**
- * Created by kmoore on 6/25/15.
- */
+import java.io.IOException;
+
 public interface ISqlDdlType extends IType, IHasInnerClass, IFileBasedType {
-    DDL getSqlSource();
+  String getSqlSource() throws IOException;
 }
